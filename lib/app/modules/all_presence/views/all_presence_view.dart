b896737bb5331e48fcbe5e744e37f51df75c006a
iqbalsuwandi20 <iqbalsuwandi20@gmail.com>
2024-10-22 17:07:18 +0700

    berhasil membuat all detail presence

--- lib/app/modules/all_presence/views/all_presence_view.dart
+++ lib/app/modules/all_presence/views/all_presence_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -43,74 +44,109 @@ class AllPresenceView extends GetView<AllPresenceController> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(left: 30, right: 30, bottom: 30),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Material(
-                    color: Colors.grey[400],
-                    borderRadius: BorderRadius.circular(20),
-                    child: InkWell(
-                      onTap: () {
-                        Get.toNamed(Routes.DETAIL_PRESENCE);
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Align(
-                              alignment: Alignment.topRight,
-                              child: Text(
-                                DateFormat.yMMMMEEEEd('id_ID')
-                                    .format(DateTime.now()),
-                                style: TextStyle(
-                                  color: Colors.green[900],
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              "Masuk",
-                              style: TextStyle(
-                                color: Colors.green[900],
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              DateFormat.jms().format(DateTime.now()),
-                              style: TextStyle(color: Colors.green[900]),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              "Keluar",
-                              style: TextStyle(
-                                color: Colors.green[900],
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              DateFormat.jms().format(DateTime.now()),
-                              style: TextStyle(color: Colors.green[900]),
-                            ),
-                          ],
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream: controller.streamAllPresence(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.green[900],
+                      ),
+                    );
+                  }
+                  if (snapshot.data!.docs.isEmpty || snapshot.data == null) {
+                    return SizedBox(
+                      height: 150,
+                      child: Center(
+                        child: Text(
+                          "Belum ada riyawat absensi",
+                          style: TextStyle(color: Colors.green[900]),
                         ),
                       ),
-                    ),
-                  ),
-                );
-              },
-            ),
+                    );
+                  }
+                  return ListView.builder(
+                    padding: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> data =
+                          snapshot.data!.docs[index].data();
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Material(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.circular(20),
+                          child: InkWell(
+                            onTap: () {
+                              Get.toNamed(
+                                Routes.DETAIL_PRESENCE,
+                                arguments: data,
+                              );
+                            },
+                            child: Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Align(
+                                    alignment: Alignment.topRight,
+                                    child: Text(
+                                      DateFormat.yMMMMEEEEd('id_ID')
+                                          .format(DateTime.parse(data["date"])),
+                                      style: TextStyle(
+                                        color: Colors.green[900],
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    "Masuk",
+                                    style: TextStyle(
+                                      color: Colors.green[900],
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    data["masuk"]?["clock"] == null
+                                        ? "-"
+                                        : DateFormat.jms().format(
+                                            DateTime.parse(
+                                                data["masuk"]!["clock"])),
+                                    style: TextStyle(color: Colors.green[900]),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    "Keluar",
+                                    style: TextStyle(
+                                      color: Colors.green[900],
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    data["keluar"]?["clock"] == null
+                                        ? "-"
+                                        : DateFormat.jms().format(
+                                            DateTime.parse(
+                                                data["keluar"]!["clock"])),
+                                    style: TextStyle(color: Colors.green[900]),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }),
           ),
         ],
       ),
