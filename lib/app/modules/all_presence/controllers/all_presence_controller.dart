80ce6a575e43a86a179eff4bbfc9549529c3bc8e
iqbalsuwandi20 <iqbalsuwandi20@gmail.com>
2024-10-23 16:51:10 +0700

    berhasil membuat filtered presence

--- lib/app/modules/all_presence/controllers/all_presence_controller.dart
+++ lib/app/modules/all_presence/controllers/all_presence_controller.dart
@@ -6,14 +6,38 @@ class AllPresenceController extends GetxController {
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Stream<QuerySnapshot<Map<String, dynamic>>> streamAllPresence() async* {
+  DateTime? start;
+  DateTime end = DateTime.now();
+
+  Future<QuerySnapshot<Map<String, dynamic>>> getAllPresence() async {
     String uid = auth.currentUser!.uid;
 
-    yield* firestore
-        .collection("pegawai")
-        .doc(uid)
-        .collection("presence")
-        .orderBy("date", descending: true)
-        .snapshots();
+    if (start == null) {
+      return await firestore
+          .collection("pegawai")
+          .doc(uid)
+          .collection("presence")
+          .where("date", isLessThan: end.toIso8601String())
+          .orderBy("date", descending: true)
+          .get();
+    } else {
+      return await firestore
+          .collection("pegawai")
+          .doc(uid)
+          .collection("presence")
+          .where("date", isGreaterThan: start!.toIso8601String())
+          .where("date",
+              isLessThan: end.add(Duration(days: 1)).toIso8601String())
+          .orderBy("date", descending: true)
+          .get();
+    }
+  }
+
+  void pickDate(DateTime pickStart, DateTime pickEnd) {
+    start = pickStart;
+    end = pickEnd;
+
+    update();
+    Get.back();
   }
 }
--- lib/app/modules/all_presence/views/all_presence_view.dart
+++ lib/app/modules/all_presence/views/all_presence_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 import '../../../routes/app_pages.dart';
 import '../controllers/all_presence_controller.dart';
@@ -21,134 +22,140 @@ class AllPresenceView extends GetView<AllPresenceController> {
         leading: SizedBox(),
         backgroundColor: Colors.green[900],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Card(
-              elevation: 8,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(
-                autocorrect: false,
-                cursorColor: Colors.green[900],
-                decoration: InputDecoration(
-                  labelText: "Cari presensi anda..",
-                  labelStyle: TextStyle(color: Colors.green[900]),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                stream: controller.streamAllPresence(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.green[900],
+      body: GetBuilder<AllPresenceController>(
+        builder: (c) {
+          return FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              future: controller.getAllPresence(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.green[900],
+                    ),
+                  );
+                }
+                if (snapshot.data!.docs.isEmpty || snapshot.data == null) {
+                  return SizedBox(
+                    height: 150,
+                    child: Center(
+                      child: Text(
+                        "Belum ada riyawat absensi",
+                        style: TextStyle(color: Colors.green[900]),
                       ),
-                    );
-                  }
-                  if (snapshot.data!.docs.isEmpty || snapshot.data == null) {
-                    return SizedBox(
-                      height: 150,
-                      child: Center(
-                        child: Text(
-                          "Belum ada riyawat absensi",
-                          style: TextStyle(color: Colors.green[900]),
-                        ),
-                      ),
-                    );
-                  }
-                  return ListView.builder(
-                    padding: EdgeInsets.only(left: 30, right: 30, bottom: 30),
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      Map<String, dynamic> data =
-                          snapshot.data!.docs[index].data();
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: Material(
-                          color: Colors.grey[400],
-                          borderRadius: BorderRadius.circular(20),
-                          child: InkWell(
-                            onTap: () {
-                              Get.toNamed(
-                                Routes.DETAIL_PRESENCE,
-                                arguments: data,
-                              );
-                            },
-                            child: Container(
-                              padding: EdgeInsets.all(15),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Align(
-                                    alignment: Alignment.topRight,
-                                    child: Text(
-                                      DateFormat.yMMMMEEEEd('id_ID')
-                                          .format(DateTime.parse(data["date"])),
-                                      style: TextStyle(
-                                        color: Colors.green[900],
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  Text(
-                                    "Masuk",
+                    ),
+                  );
+                }
+                return ListView.builder(
+                  padding: EdgeInsets.all(30),
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic> data =
+                        snapshot.data!.docs[index].data();
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Material(
+                        color: Colors.grey[400],
+                        borderRadius: BorderRadius.circular(20),
+                        child: InkWell(
+                          onTap: () {
+                            Get.toNamed(
+                              Routes.DETAIL_PRESENCE,
+                              arguments: data,
+                            );
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    DateFormat.yMMMMEEEEd('id_ID')
+                                        .format(DateTime.parse(data["date"])),
                                     style: TextStyle(
                                       color: Colors.green[900],
                                       fontWeight: FontWeight.bold,
                                     ),
                                   ),
-                                  Text(
-                                    data["masuk"]?["clock"] == null
-                                        ? "-"
-                                        : DateFormat.jms().format(
-                                            DateTime.parse(
-                                                data["masuk"]!["clock"])),
-                                    style: TextStyle(color: Colors.green[900]),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Masuk",
+                                  style: TextStyle(
+                                    color: Colors.green[900],
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  SizedBox(
-                                    height: 10,
+                                ),
+                                Text(
+                                  data["masuk"]?["clock"] == null
+                                      ? "-"
+                                      : DateFormat.jms().format(DateTime.parse(
+                                          data["masuk"]!["clock"])),
+                                  style: TextStyle(color: Colors.green[900]),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "Keluar",
+                                  style: TextStyle(
+                                    color: Colors.green[900],
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  Text(
-                                    "Keluar",
-                                    style: TextStyle(
-                                      color: Colors.green[900],
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    data["keluar"]?["clock"] == null
-                                        ? "-"
-                                        : DateFormat.jms().format(
-                                            DateTime.parse(
-                                                data["keluar"]!["clock"])),
-                                    style: TextStyle(color: Colors.green[900]),
-                                  ),
-                                ],
-                              ),
+                                ),
+                                Text(
+                                  data["keluar"]?["clock"] == null
+                                      ? "-"
+                                      : DateFormat.jms().format(DateTime.parse(
+                                          data["keluar"]!["clock"])),
+                                  style: TextStyle(color: Colors.green[900]),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      );
-                    },
-                  );
-                }),
-          ),
-        ],
+                      ),
+                    );
+                  },
+                );
+              });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green[900],
+        onPressed: () {
+          Get.dialog(Dialog(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              height: 400,
+              color: Colors.green[900],
+              child: SfDateRangePicker(
+                monthViewSettings:
+                    DateRangePickerMonthViewSettings(firstDayOfWeek: 1),
+                selectionMode: DateRangePickerSelectionMode.range,
+                showActionButtons: true,
+                onCancel: () => Get.back(),
+                onSubmit: (p0) {
+                  if (p0 != null) {
+                    if ((p0 as PickerDateRange).endDate != null) {
+                      controller.pickDate(p0.startDate!, p0.endDate!);
+                    }
+                  }
+                },
+              ),
+            ),
+          ));
+        },
+        child: Icon(
+          Icons.format_list_bulleted_outlined,
+          color: Colors.white,
+        ),
       ),
     );
   }
